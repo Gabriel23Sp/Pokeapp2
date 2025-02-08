2ce1d0a111a9d2f9ef002e622e2319027d1c6e37
Gabriel <astl5526@gmail.com>
2025-02-07 21:35:34 -0500

    first commit

---
+++ src/app/components/Users.tsx
@@ -0,0 +1,29 @@
+export const Users =[
+    {
+        nombre: "Juan",
+        apellido: "Pérez",
+        nickname: "juanp",
+        correo: "juanp@example.com",
+        password: "password1",
+        fechaNacimiento: "1990-05-12",
+        telefono: "1234567890"
+      },
+      {
+        nombre: "María",
+        apellido: "Gómez",
+        nickname: "mariag",
+        correo: "mariag@example.com",
+        password: "password2",
+        fechaNacimiento: "1988-09-25",
+        telefono: "0987654321"
+      },
+      {
+        nombre: "Carlos",
+        apellido: "López",
+        nickname: "carlitos",
+        correo: "carlitos@example.com",
+        password: "password3",
+        fechaNacimiento: "1995-07-15",
+        telefono: "1122334455"
+      }
+]
